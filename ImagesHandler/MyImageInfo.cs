9730c8ea09c0975e86edeb92f370c253fe92f9c7
smachew <smachew@weizmann.ac.il>
2020-02-02 15:24:26 +0200

    In Progg.

---
+++ ImagesHandler/MyImageInfo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagesHandler
+{
+    public class MyImageInfo
+    {
+        public string iPath { get; set; }
+
+        public string FileName { get; set; }
+
+        public Bitmap iBitmap { get; set; }
+
+        public int currentDifrences { get; set; }
+
+        public bool? IsUnique { get; set; }
+
+        public MyImageInfo(string path)
+        {
+            try
+            {
+                iPath = path;
+                iBitmap = new Bitmap(path);
+                Clones = new List<MyImageInfo>();
+                FileName = Path.GetFileName(path);
+            }
+            catch (Exception ex)
+            {
+                var xxc = ex.Message;
+            }
+        }
+
+        private void Comapre(MyImageInfo other)
+        {
+            var thisfn = FileName;
+            var otherfn = other.FileName;
+
+            bool equals = true;
+            Rectangle rect1 = new Rectangle(0, 0, iBitmap.Width, iBitmap.Height);
+            Rectangle rect2 = new Rectangle(0, 0, other.iBitmap.Width, other.iBitmap.Height);
+            if (rect1.Width == rect2.Width && rect1.Height == rect2.Height)
+            {
+                BitmapData bmpData1 = iBitmap.LockBits(rect1, ImageLockMode.ReadOnly, iBitmap.PixelFormat);
+                BitmapData bmpData2 = other.iBitmap.LockBits(rect1, ImageLockMode.ReadOnly, other.iBitmap.PixelFormat);
+                unsafe
+                {
+                    byte* ptr1 = (byte*)bmpData1.Scan0.ToPointer();
+                    byte* ptr2 = (byte*)bmpData2.Scan0.ToPointer();
+                    int width = rect1.Width * 3; // for 24bpp pixel data
+                    for (int y = 0; equals && y < rect1.Height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            if (*ptr1 != *ptr2)
+                            {
+                                equals = false;
+                                break;
+                            }
+                            ptr1++;
+                            ptr2++;
+                        }
+                        ptr1 += bmpData1.Stride - width;
+                        ptr2 += bmpData2.Stride - width;
+                    }
+                }
+                iBitmap.UnlockBits(bmpData1);
+                other.iBitmap.UnlockBits(bmpData2);
+
+                if (equals)
+                {
+                    Clones.Add(other);
+                    other.Clones.Add(this);
+                }
+            }
+        }
+
+        public void ScanImages(List<MyImageInfo> images)
+        {
+            foreach (var image in images)
+            {
+                if (!image.IsUnique.HasValue)
+                {
+                    Comapre(image);
+                }
+            }
+            if (Clones.Count > 0)
+                IsUnique = true;
+        }
+
+        public List<MyImageInfo> Clones { get; set; }
+    }
+}
---
+++ ImagesHandlerTester/Program.cs
@@ -0,0 +1,51 @@
+﻿using ImagesHandler;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagesHandlerTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                var path = @"C:\Users\smachew.WISMAIN\Desktop\tmp\testimages";
+                string supportedExtensions = "*.jpg,*.gif,*.png,*.bmp,*.jpeg";
+
+                var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(s => supportedExtensions.Contains(Path.GetExtension(s).ToLower()));
+                var mIs = files.Select(x => new MyImageInfo(x)).ToList();
+                var currentIdx = 0;
+                do
+                {
+                    for (int i = 0; i < mIs.Count; i++)
+                    {
+                        var currentMi = mIs[currentIdx];
+                        currentMi.ScanImages(mIs.GetRange(currentIdx + 1, mIs.Count - currentIdx - 1));
+                    }
+                    currentIdx++;
+                }
+                while (currentIdx < mIs.Count);
+
+                //foreach (var mi in mIs)
+                //{
+                //    var actives = mIs.Where(x => x.IsUnique != true && x != mi).ToList();
+                //    mi.ScanImages(actives);
+                //}
+                var cnt = mIs.Where(x => x.IsUnique == true).Count();
+                Console.WriteLine(cnt);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
